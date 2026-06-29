f0c0ca06d6cc32211441ffeb2939f498841b8ddb
saravanan <dhanushsaravana21@gmail.com>
2026-06-29 10:31:24 +0530

    day-7

--- Day-6-Custom-Network/main.tf
+++ Day-6-Custom-Network/main.tf
@@ -34,8 +34,8 @@ resource "aws_internet_gateway" "igw" {
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
 
-  route = {
-    cidr_block = "0.0.0.0"
+  route  {
+    cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igw.id
   }
   tags = {
